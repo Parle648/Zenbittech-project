1e5840a8d21501eba3f0170c33a18aeae2703983
Parle648 <sashahoumi@gmail.com>
2024-09-26 18:24:48 +0300

    + Create loading slice

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import token from '../features/slcies/token-slice/tokenSlice';
+import loading from '../features/slcies/loading-slice/loading';
 
 const stateStore = configureStore({
   reducer: {
     token: token,
+    loading: loading,
   },
 });
 
