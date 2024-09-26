efdf038da62c1527434898bc1c36b617e14669f3
Parle648 <sashahoumi@gmail.com>
2024-09-26 15:53:12 +0300

    + Create backend

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DbModule } from './db/db.module';
-import { DbService } from './db/db.service';
+import { DatabaseService } from './database/database.service';
+import { DealsModule } from './deals/deals.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DealsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService, DbService],
+  providers: [AppService, DatabaseService, UsersService],
 })
 export class AppModule {}
