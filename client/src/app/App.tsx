f865efbcc49d450fc806971abbb41e4f76389d3b
Parle648 <sashahoumi@gmail.com>
2024-09-26 16:07:29 +0300

    + Create routing for client

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,10 +1,19 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// todo lazy loading
+import Main from '../pages/Main';
+import SignUp from '../pages/SignUp';
+import Login from '../pages/Login';
 
+function App() {
   return (
-    <>
-      App
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
