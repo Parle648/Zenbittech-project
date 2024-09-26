4d5c8df5361c357b4e50b81174e060e05f9b195e
Parle648 <sashahoumi@gmail.com>
2024-09-26 17:19:30 +0300

    + Create UI for SignUpForm

--- client/src/features/SignUpForm/SignUpForm.tsx
+++ client/src/features/SignUpForm/SignUpForm.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { useForm } from 'react-hook-form';
 import IForm from './types/IForm';
+import { Link } from 'react-router-dom';
+import styles from './styles/signUpForm.module.scss';
 
 const SignUpForm: FC = () => {
   const { register, handleSubmit } = useForm<IForm>();
@@ -10,15 +12,22 @@ const SignUpForm: FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(authentificate)}>
+    <form className={styles.signUpForm} onSubmit={handleSubmit(authentificate)}>
+      <h2 className={styles.formTitle}>Sign up</h2>
+      <h4 className={styles.inputTitle}>Name</h4>
       <input
+        className={styles.input}
         type="text"
+        placeholder="name"
         {...register('name', {
           required: true,
         })}
       />
+      <h4 className={styles.inputTitle}>Email</h4>
       <input
+        className={styles.input}
         type="text"
+        placeholder="example@gmail.com"
         {...register('email', {
           required: true,
           pattern: {
@@ -27,8 +36,11 @@ const SignUpForm: FC = () => {
           },
         })}
       />
+      <h4 className={styles.inputTitle}>password</h4>
       <input
+        className={styles.input}
         type="text"
+        placeholder="password"
         {...register('password', {
           required: true,
           minLength: {
@@ -37,6 +49,13 @@ const SignUpForm: FC = () => {
           },
         })}
       />
+      <input className={styles.submitBtn} type="submit" value="Sign up" />
+      <div className={styles.loginPrompt}>
+        Already have an accont?{' '}
+        <Link className={styles.loginLink} to="/login">
+          Login
+        </Link>
+      </div>
     </form>
   );
 };
